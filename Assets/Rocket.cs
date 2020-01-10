9ccf6f3a869bccb5dd289c73fd2babab5c1dd336
Christian Halsell <christian.halsell@verys.com>
2020-01-10 10:08:33 -0800

    Added platforms and rotation fix

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -18,10 +18,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
@@ -36,8 +37,13 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
 
-        if (Input.GetKey(KeyCode.A)) 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
         }
@@ -45,5 +51,7 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }

c72ec9248369e721b0a6206022f958fddb3ce9e7
Christian Halsell <christian.halsell@verys.com>
2020-01-16 16:24:25 -0800

    Added scene management

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -30,13 +28,14 @@ public class Rocket : MonoBehaviour
         switch(collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");  //TODO remove
                 break;
-            case "Fuel":
-                print("Fuel"); //TODO remove
+            case "Finish":
+                print("Hit Finish"); //TODO remove
+                SceneManager.LoadScene(1);
                 break;
             default:
-                print("dead!");
+                print("Dead!");
+                SceneManager.LoadScene(0);
                 break;
         }
     }

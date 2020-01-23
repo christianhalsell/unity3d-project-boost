86192ef0ad78451195e5909d96c2cbdca4689ab6
Christian Halsell <christian.halsell@verys.com>
2020-01-22 18:45:39 -0800

    Made items move automatically

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
-    // TODO: remove from inspector later
+    // TODO remove from inspector later
     [Range(0,1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved
 
     Vector3 startingPos;
@@ -21,6 +22,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // TODO protect against period is zero
+        float cycles = Time.time / period; // grows continually from zero
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f; 
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
